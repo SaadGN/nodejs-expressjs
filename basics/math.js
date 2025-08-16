2af85a5f8ab11a751bd450908f7cb5795131ab90
saad <saadgn89@gmail.com>
2025-08-16 21:49:48 +0500

    file for math function for modular code

---
+++ basics/math.js
@@ -0,0 +1,14 @@
+function add(a,b){
+    return a+b
+}
+function sub(a,b){
+    return a-b
+}
+
+module.exports = {
+    add , sub
+}
+
+exports.product = (a,b) => a*b;
+
+exports.div = (a,b) => a/b;
