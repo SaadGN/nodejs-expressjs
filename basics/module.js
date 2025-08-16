43ea67cc0338808b1549f1bcf97fe6835fa10a08
saad <saadgn89@gmail.com>
2025-08-16 21:49:07 +0500

    modules in js (export and import)

---
+++ basics/module.js
@@ -0,0 +1,11 @@
+console.log("Node Javascript")
+
+
+const math = require('./math')
+
+console.log("Sum  :", math.add(3,4))
+console.log("sub  :", math.sub(3,4))
+
+
+// console.log("\nMultiply  :", math.product(3,3))
+// console.log("Divide  :", math.div(3,2))
