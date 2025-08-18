d31a34311cc801618add284036600d45bcd99571
saad <saadgn89@gmail.com>
2025-08-18 18:43:25 +0500

    expressjs used

--- server/index.js
+++ server/index.js
@@ -1,51 +1,61 @@
 const http = require('http')
 
-const fs = require('fs')
+// const fs = require('fs')
 
-const url = require('url')
+// const url = require('url')
+
+const express = require('express')
+const app = express();
+
 // create server 
-
 //handling URLs
+// function myhandler(req,res){
+    
+//     if (req.url === "/favicon.ico") return res.end();
 
-const myServer = http.createServer((req, res) => {
+//     const log = `${Date.now()} : ${req.url}  , ${req.method}  ,  new request recieved\n`
 
-    if (req.url === "/favicon.ico") return res.end();
+//     const myUrl = url.parse(req.url, true);
+//     //  console.log(myUrl)
 
-    const log = `${Date.now()} : ${req.url}  , ${req.method}  ,  new request recieved\n`
+//     fs.appendFile('log.txt', log, (err, data) => {
 
-    const myUrl = url.parse(req.url,true);
-  //  console.log(myUrl)
+//         switch (myUrl.pathname) {
+//             case '/':
+//                 res.end("HomePage")
+//                 break;
 
-    fs.appendFile('log.txt', log, (err, data) => {
+//             case '/about':
+//                 const username = myUrl.query.myname;
+//                 res.end(`Hi from ,${username}`)
+//                 break;
 
-        switch (myUrl.pathname) {
-            case '/':
-                res.end("HomePage")
-                break;
+//             case '/signup':
+//                 if (req.method === 'GET') {
+//                     res.end('This is a signup form ')
+//                 } else if (req.methond === 'POST') {
+//                     //DB query
+//                     req.end('Succes form submitted')
+//                 }
+//                 break;
 
-            case '/about': 
-            const username = myUrl.query.myname;
-            res.end(`Hi from ,${username}`)
-                break;
+//             default:
+//                 res.end("404 Not Found")
+//                 break;
+//         }
+// })
+// }
+// const myServer = http.createServer(app);
 
-            case '/signup' : 
-            if(req.method === 'GET') {
-                res.end('This is a signup form ')
-            } else if(req.methond === 'POST') {
-                //DB query
-                req.end('Succes form submitted')
-            }
-            break;
-
-            default:
-                res.end("404 Not Found")
-                break;
-        }
-
-
-    });
-
-});
+// myServer.listen(8000, () => console.log("Server Started!!!"))
 
 
-myServer.listen(8000, () => console.log("Server started"))
+app.get("/",(req,res) => {
+    return res.end("Hello from HomePage"+ "\nHey " + req.query.name)
+})
+app.get("/about",(req,res) => {
+    return res.end("Hello from about Page" + "\nHey " + req.query.name)
+})
+
+app.listen(8000 , ()=> console.log("Server Started!!!"))
+
