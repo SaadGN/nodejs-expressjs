ec59225cf39de8e01b997c48e9ea242f0ad49c0b
SaadGN <saadgn89@gmail.com>
2025-08-21 17:59:24 +0500

    integrated with mongoDB atlas

--- project_02/index.js
+++ project_02/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 
-const users = require('./MOCK_DATA.json')
+// const users = require('./MOCK_DATA.json')
 
 const fs = require('fs')
 
@@ -8,52 +8,61 @@ const app = express()
 const port = 8000
 
 const mongoose = require('mongoose')
+const { timeStamp } = require('console')
 
 //connection
 mongoose.connect('mongodb+srv://test:%23pakistan123@project1.78ta2fh.mongodb.net/project1')
-// mongodb+srv://test:%23pakistan123@project1.78ta2fh.mongodb.net/project1
-.then(()=> {
-    console.log("MongoDB Connected")
-})
-.catch((err)=> {
-    console.log("Err: ",err)
-})
+    // mongodb+srv://test:%23pakistan123@project1.78ta2fh.mongodb.net/project1
+    .then(() => {
+        console.log("MongoDB Connected")
+    })
+    .catch((err) => {
+        console.log("Err: ", err)
+    })
 
 
 // Schema
 const userSchema = new mongoose.Schema({
-    first_name: { 
+    first_name: {
         type: String, required: true
-     },
+    },
     last_name: {
-         type: String 
-        },
-    email: { 
+        type: String
+    },
+    email: {
         type: String, required: true, unique: true
-     },
-    gender: { 
-        type: String 
+    },
+    gender: {
+        type: String
     }
-});
+}, { timestamps: true });
 
 // Model
 const User = mongoose.model('User', userSchema);
 
 //MiddleWare
-app.use(express.urlencoded({ extended:false }));
+app.use(express.urlencoded({ extended: false }));
+
+
+
+
 
 // render html request
-app.get('/users', (req, res) => {
+app.get('/users', async (req, res) => {
+    const allDBUsers = await User.find({})
+
+
     const html = `
         <ul>
-            ${users.map((users) => `<li> ${users.first_name} </li>`).join("")}
+            ${allDBUsers
+            .map((user) => `<li> ${user.first_name} </li>`).join("")}
         </ul>
     `;
     res.send(html)
 })
 
 
-    // Routes
+// Routes
 
 
 /*    ``````request for same paths done separately``````
@@ -87,57 +96,86 @@ app.delete('/api/users/:id', (req, res) => {
 
 // ***GET /api/users   =>   get all users [JSON]
 
-app.get('/api/users', (req, res) => {
-    return res.json(users);
+app.get('/api/users', async (req, res) => {
+    const allDBUsers = await User.find({})
+    // res.setHeaders("X-MyName, expressJS"); // custom header
+    return res.json(allDBUsers);
 })
 
 
 
 app
     .route('/api/users/:id')
-    .get((req, res) => {
-        const id = Number(req.params.id);
-        const user = users.find((user) => user.id === id)
+    .get(async (req, res) => {
+        // const id = Number(req.params.id);
+        // const user = users.find((user) => user.id === id)
+        const user = await User.findById(req.params.id)
         return res.json(user)
     })
-    .patch((req, res) => {
+    .patch(async (req, res) => {
         //TODO : update an user with id
-        return res.json({ status: "pending" })
+        await User.findByIdAndUpdate(req.params.id, {
+            last_name: "changed"
+        })
+
+        return res.json({ status: "success" })
     })
-    .delete((req, res) => {
-    const id = Number(req.params.id);
-    const index = users.findIndex((user) => user.id === id);
+    .delete(async (req, res) => {
 
-    if (index === -1) {
-        return res.status(404).json({ error: "User not found" });
-    }
 
-    // Remove user from array
-    users.splice(index, 1);
+        await User.findByIdAndDelete(req.params.id)
+        return res.json({ status: "success" });
 
-    // Update the JSON file
-    fs.writeFile('./MOCK_DATA.json', JSON.stringify(users), (err) => {
-        if (err) {
-            return res.status(500).json({ error: "Failed to delete user" });
-        }
-        return res.json({ status: "success", id });
-    });
-})
+        // const id = Number(req.params.id);
+        // const index = users.findIndex((user) => user.id === id);
+
+        // if (index === -1) {
+        //     return res.status(404).json({ error: "User not found" });
+        // }
+
+        // // Remove user from array
+        // users.splice(index, 1);
+
+        // // Update the JSON file
+        // fs.writeFile('./MOCK_DATA.json', JSON.stringify(users), (err) => {
+        //     if (err) {
+        //         return res.status(500).json({ error: "Failed to delete user" });
+        //     }
+        //     return res.json({ status: "success", id });
+        // });
+    })
 
 
 
 // ***POST /api/users   =>   create new users [JSON]***
-app.post('/api/users', (req, res) => {
+app.post('/api/users', async (req, res) => {
     const body = req.body;
+    if (
+        !body ||
+        !body.first_name ||
+        !body.last_name ||
+        !body.email ||
+        !body.gender
+    ) {
+        return res.status(400).json({ msg: "ALl fields are required" })
+    }
 
-    users.push({ id: users.length + 1, ...body });
+    // users.push({ id: users.length + 1, ...body });
 
-    fs.writeFile('./MOCK_DATA.json', JSON.stringify(users), (err) => {
-        if (err) {
-            return res.status(500).json({ status: "Error", message: "Failed to save user" });
-        }
-        return res.status(201).json({ status: "Success", id: users.length });
-    });
+    // fs.writeFile('./MOCK_DATA.json', JSON.stringify(users), (err) => {
+    //     if (err) {
+    //         return res.status(500).json({ status: "Error", message: "Failed to save user" });
+    //     }
+    //     return res.status(201).json({ status: "Success", id: users.length });
+    // });
+    const result = await User.create({
+        first_name: body.first_name,
+        last_name: body.last_name,
+        email: body.email,
+        gender: body.gender
+    })
+    // console.log("result",result)
+    return res.status(201).json({ msg: "success" })
 });
 
 
