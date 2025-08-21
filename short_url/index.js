1eac8692e2ada99d08d41a6e056d1c146a53974d
SaadGN <saadgn89@gmail.com>
2025-08-22 02:33:08 +0500

    index file after ejs

--- short_url/index.js
+++ short_url/index.js
@@ -1,8 +1,13 @@
 const express = require('express')
 
+const path = require("path")
+
 const { connectToMongoDB } = require('./connect')
 
+const staticRoute = require('./routes/staticRouter')
+
 const urlRoute = require('./routes/url');
+
 
 const URL = require("./models/url")
 
@@ -13,9 +18,22 @@ const PORT = 8001
 connectToMongoDB('mongodb+srv://saadgn789:Y7wlOfYGEMT08t81@task2.oatn6bb.mongodb.net/')
     .then(() => console.log("MongoDB connected"))
 
+app.set("view engine","ejs")
+
+app.set("views",path.resolve('./views') )
+
 app.use(express.json())
+app.use(express.urlencoded({extended : false    }))
+// app.get('/test', async (req,res) => {
+//     const allUrls = await URL.find({});
+//     return res.render("home",{
+//         urls : allUrls
+//     })
+// })
 
 app.use("/url", urlRoute)
+
+app.use("/",staticRoute)
 
 app.get("/:shortId", async (req, res) => {
     const shortId = req.params.shortId;
