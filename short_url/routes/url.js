b97fab65c96ba3d9c1457645913238ba78c24280
SaadGN <saadgn89@gmail.com>
2025-08-22 20:49:42 +0500

    url route updated for auth

--- short_url/routes/url.js
+++ short_url/routes/url.js
@@ -1,13 +1,13 @@
-const express = require('express')
-
-const {handleGenerateNewShortURL , handleGetAnalytics }  = require('../controllers/url')
+const express = require("express");
+const {
+  handleGenerateNewShortURL,
+  handleGetAnalytics,
+} = require("../controllers/url");
 
 const router = express.Router();
 
+router.post("/", handleGenerateNewShortURL);
 
+router.get("/analytics/:shortId", handleGetAnalytics);
 
-router.post('/',handleGenerateNewShortURL)
-
-router.get("/analytics/:shortId", handleGetAnalytics )
-
-module.exports = router
+module.exports = router;
