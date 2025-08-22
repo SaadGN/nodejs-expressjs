c78296058f9f5fb91fcd7b3850c24c862272f199
SaadGN <saadgn89@gmail.com>
2025-08-22 20:48:30 +0500

    stateful authentication with cookies

--- short_url/index.js
+++ short_url/index.js
@@ -1,57 +1,46 @@
-const express = require('express')
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const { connectToMongoDB } = require("./connect");
 
-const path = require("path")
+const { restrictToLoggedinUserOnly, checkAuth } = require("./middlewares/auth");
+const URL = require("./models/url");
 
-const { connectToMongoDB } = require('./connect')
+const urlRoute = require("./routes/url");
+const staticRoute = require("./routes/staticRouter");
+const userRoute = require("./routes/user");
 
-const URL = require("./models/url")
-
-const staticRoute = require('./routes/staticRouter')
-
-const urlRoute = require('./routes/url');
-const userRoute = require('./routes/user')
-
-const app = express()
-
-const PORT = 8001
+const app = express();
+const PORT = 8001;
 
 connectToMongoDB('mongodb+srv://saadgn789:Y7wlOfYGEMT08t81@task2.oatn6bb.mongodb.net/')
     .then(() => console.log("MongoDB connected"))
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
 
-app.set("view engine","ejs")
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
-app.set("views",path.resolve('./views') )
+app.use("/url", restrictToLoggedinUserOnly, urlRoute);
+app.use("/user", userRoute);
+app.use("/", checkAuth, staticRoute);
 
-app.use(express.json())
-app.use(express.urlencoded({extended : false    }))
-// app.get('/test', async (req,res) => {
-//     const allUrls = await URL.find({});
-//     return res.render("home",{
-//         urls : allUrls
-//     })
-// })
-
-app.use("/url", urlRoute)
-app.use('/user',userRoute)
-app.use("/",staticRoute)
-
-app.get("/:shortId", async (req, res) => {
-    const shortId = req.params.shortId;
-    const entry = await URL.findOneAndUpdate(
-        { shortId },
-        {
-            $push: {
-                visitHistory: {
-                    timestamp: Date.now()  },
-            },
+app.get("/url/:shortId", async (req, res) => {
+  const shortId = req.params.shortId;
+  const entry = await URL.findOneAndUpdate(
+    {
+      shortId,
+    },
+    {
+      $push: {
+        visitHistory: {
+          timestamp: Date.now(),
         },
-       { new: true } // return updated doc);
-    );
-     if (!entry) {
-        return res.status(404).json({ error: "Short URL not found" });
+      },
     }
-    res.redirect(entry.redirectURL)
+  );
+  res.redirect(entry.redirectURL);
 });
 
-app.listen(PORT, () => console.log("Server started at PORT 8001"))
-
+app.listen(PORT, () => console.log(`Server Started at PORT:${PORT}`));
