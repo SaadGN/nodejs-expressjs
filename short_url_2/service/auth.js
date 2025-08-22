f7efdec3cc6b09350c8b193202b3f5f1b657c5ee
SaadGN <saadgn89@gmail.com>
2025-08-23 01:07:48 +0500

    authorization

---
+++ short_url_2/service/auth.js
@@ -0,0 +1,26 @@
+
+const jwt = require("jsonwebtoken")
+const secret = "123@123"
+
+function setUser(user) {
+  const payload = { _id: user._id, email: user.email };
+  return jwt.sign(payload, secret, { expiresIn: "1h" });
+  // return jwt.sign(user,secret);
+}
+
+function getUser(token) {
+  if (!token) return null;
+
+  try {
+    return jwt.verify(token, secret);
+  } catch (err) {
+   // console.error("JWT verification failed:", err.message);
+    return null;
+  }
+  // return jwt.verify(token,secret)
+}
+
+module.exports = {
+  setUser,
+  getUser,
+};
